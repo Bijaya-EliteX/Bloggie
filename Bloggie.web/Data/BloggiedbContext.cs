e68f4f462dd21e1451cde68f83bbb2c311f28149
bijaya <bijayatiwari31@gmail.com>
2025-09-04 19:50:32 +0545

    First sync

---
+++ Bloggie.web/Data/BloggiedbContext.cs
@@ -0,0 +1,13 @@
+﻿using Bloggie.web.Models.Domain;    
+using Microsoft.EntityFrameworkCore;
+namespace Bloggie.web.Data
+{
+    public class BloggiedbContext:DbContext
+    {
+        public BloggiedbContext(DbContextOptions options):base(options)
+        {
+        }
+        public DbSet<BlogPost> Blogposts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
