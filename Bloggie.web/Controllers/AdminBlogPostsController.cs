14f6011c5e9a2d01b42b0fa840bc93cedf26c017
bijaya <bijayatiwari31@gmail.com>
2025-10-13 18:45:54 +0545

    BlogPostController add

---
+++ Bloggie.web/Controllers/AdminBlogPostsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace Bloggie.web.Controllers
+{
+    public class AdminBlogPostsController: Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
